7886c641a7fe271b6ad4e50ba87661905826edb2
Kristian Schlikow <kristian@schlikow.de>
2021-07-13 03:19:29 +0200

    Add vanilla class implementations as references for the future

---
+++ Source/NUIT/Vanilla/ITab_Storage.cs
@@ -0,0 +1,107 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: RimWorld.ITab_Storage
+// Assembly: Assembly-CSharp, Version=1.2.7810.35712, Culture=neutral, PublicKeyToken=null
+// MVID: 7927E938-DE4C-4089-B4AD-D4215E58D5B1
+// Assembly location: G:\SteamLibrary\steamapps\common\RimWorld\RimWorldWin64_Data\Managed\Assembly-CSharp.dll
+
+// THIS FILE IS SOLELY MEANT AS REFERENCE
+// NO OWNERSHIP IS CLAIMED, ALL RIGHTS BELONG TO LUDEON STUDIOS
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace RimWorld
+{
+  public class ITab_Storage : ITab
+  {
+    private Vector2 scrollPosition;
+    private static readonly Vector2 WinSize = new Vector2(300f, 480f);
+
+    protected virtual IStoreSettingsParent SelStoreSettingsParent => this.SelObject is Thing selObject ? this.GetThingOrThingCompStoreSettingsParent(selObject) ?? (IStoreSettingsParent) null : this.SelObject as IStoreSettingsParent;
+
+    public override bool IsVisible
+    {
+      get
+      {
+        if (this.SelObject is Thing selObject && selObject.Faction != null && selObject.Faction != Faction.OfPlayer)
+          return false;
+        IStoreSettingsParent storeSettingsParent = this.SelStoreSettingsParent;
+        return storeSettingsParent != null && storeSettingsParent.StorageTabVisible;
+      }
+    }
+
+    protected virtual bool IsPrioritySettingVisible => true;
+
+    private float TopAreaHeight => this.IsPrioritySettingVisible ? 35f : 20f;
+
+    public ITab_Storage()
+    {
+      this.size = ITab_Storage.WinSize;
+      this.labelKey = "TabStorage";
+      this.tutorTag = "Storage";
+    }
+
+    protected override void FillTab()
+    {
+      IStoreSettingsParent storeSettingsParent = this.SelStoreSettingsParent;
+      StorageSettings settings = storeSettingsParent.GetStoreSettings();
+      Rect position = new Rect(0.0f, 0.0f, ITab_Storage.WinSize.x, ITab_Storage.WinSize.y).ContractedBy(10f);
+      GUI.BeginGroup(position);
+      if (this.IsPrioritySettingVisible)
+      {
+        Text.Font = GameFont.Small;
+        Rect rect = new Rect(0.0f, 0.0f, 160f, this.TopAreaHeight - 6f);
+        if (Widgets.ButtonText(rect, (string) ("Priority".Translate() + ": " + settings.Priority.Label().CapitalizeFirst())))
+        {
+          List<FloatMenuOption> options = new List<FloatMenuOption>();
+          foreach (StoragePriority storagePriority in Enum.GetValues(typeof (StoragePriority)))
+          {
+            if (storagePriority != StoragePriority.Unstored)
+            {
+              StoragePriority localPr = storagePriority;
+              options.Add(new FloatMenuOption(localPr.Label().CapitalizeFirst(), (Action) (() => settings.Priority = localPr)));
+            }
+          }
+          Find.WindowStack.Add((Window) new FloatMenu(options));
+        }
+        UIHighlighter.HighlightOpportunity(rect, "StoragePriority");
+      }
+      ThingFilter thingFilter = (ThingFilter) null;
+      if (storeSettingsParent.GetParentStoreSettings() != null)
+        thingFilter = storeSettingsParent.GetParentStoreSettings().filter;
+      Rect rect1 = new Rect(0.0f, this.TopAreaHeight, position.width, position.height - this.TopAreaHeight);
+      Bill[] array1 = BillUtility.GlobalBills().Where<Bill>((Func<Bill, bool>) (b => b is Bill_Production && b.GetStoreZone() == storeSettingsParent && b.recipe.WorkerCounter.CanPossiblyStoreInStockpile((Bill_Production) b, b.GetStoreZone()))).ToArray<Bill>();
+      ref Vector2 local = ref this.scrollPosition;
+      ThingFilter filter = settings.filter;
+      ThingFilter parentFilter = thingFilter;
+      ThingFilterUI.DoThingFilterConfigWindow(rect1, ref local, filter, parentFilter, 8);
+      Bill[] array2 = BillUtility.GlobalBills().Where<Bill>((Func<Bill, bool>) (b => b is Bill_Production && b.GetStoreZone() == storeSettingsParent && b.recipe.WorkerCounter.CanPossiblyStoreInStockpile((Bill_Production) b, b.GetStoreZone()))).ToArray<Bill>();
+      foreach (Bill bill in ((IEnumerable<Bill>) array1).Except<Bill>((IEnumerable<Bill>) array2))
+        Messages.Message((string) "MessageBillValidationStoreZoneInsufficient".Translate((NamedArgument) bill.LabelCap, (NamedArgument) bill.billStack.billGiver.LabelShort.CapitalizeFirst(), (NamedArgument) bill.GetStoreZone().label), (LookTargets) (bill.billStack.billGiver as Thing), MessageTypeDefOf.RejectInput, false);
+      PlayerKnowledgeDatabase.KnowledgeDemonstrated(ConceptDefOf.StorageTab, KnowledgeAmount.FrameDisplayed);
+      GUI.EndGroup();
+    }
+
+    protected IStoreSettingsParent GetThingOrThingCompStoreSettingsParent(
+      Thing t)
+    {
+      switch (t)
+      {
+        case IStoreSettingsParent storeSettingsParent2:
+          return storeSettingsParent2;
+        case ThingWithComps thingWithComps:
+          List<ThingComp> allComps = thingWithComps.AllComps;
+          for (int index = 0; index < allComps.Count; ++index)
+          {
+            if (allComps[index] is IStoreSettingsParent storeSettingsParent3)
+              return storeSettingsParent3;
+          }
+          break;
+      }
+      return (IStoreSettingsParent) null;
+    }
+  }
+}
---
+++ Source/NUIT/Vanilla/ThingFilterUI.cs
@@ -0,0 +1,105 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Verse.ThingFilterUI
+// Assembly: Assembly-CSharp, Version=1.2.7810.35712, Culture=neutral, PublicKeyToken=null
+// MVID: 7927E938-DE4C-4089-B4AD-D4215E58D5B1
+// Assembly location: G:\SteamLibrary\steamapps\common\RimWorld\RimWorldWin64_Data\Managed\Assembly-CSharp.dll
+
+// THIS FILE IS SOLELY MEANT AS REFERENCE
+// NO OWNERSHIP IS CLAIMED, ALL RIGHTS BELONG TO LUDEON STUDIOS
+
+using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse.Sound;
+
+namespace Verse
+{
+  public static class ThingFilterUI
+  {
+    private static float viewHeight;
+    private const float ExtraViewHeight = 90f;
+    private const float RangeLabelTab = 10f;
+    private const float RangeLabelHeight = 19f;
+    private const float SliderHeight = 28f;
+    private const float SliderTab = 20f;
+
+    public static void DoThingFilterConfigWindow(
+      Rect rect,
+      ref Vector2 scrollPosition,
+      ThingFilter filter,
+      ThingFilter parentFilter = null,
+      int openMask = 1,
+      IEnumerable<ThingDef> forceHiddenDefs = null,
+      IEnumerable<SpecialThingFilterDef> forceHiddenFilters = null,
+      bool forceHideHitPointsConfig = false,
+      List<ThingDef> suppressSmallVolumeTags = null,
+      Map map = null)
+    {
+      Widgets.DrawMenuSection(rect);
+      Text.Font = GameFont.Tiny;
+      float num1 = rect.width - 2f;
+      Rect rect1 = new Rect(rect.x + 1f, rect.y + 1f, num1 / 2f, 24f);
+      if (Widgets.ButtonText(rect1, (string) "ClearAll".Translate()))
+      {
+        filter.SetDisallowAll(forceHiddenDefs, forceHiddenFilters);
+        SoundDefOf.Checkbox_TurnedOff.PlayOneShotOnCamera();
+      }
+      if (Widgets.ButtonText(new Rect(rect1.xMax + 1f, rect1.y, (float) ((double) rect.xMax - 1.0 - ((double) rect1.xMax + 1.0)), 24f), (string) "AllowAll".Translate()))
+      {
+        filter.SetAllowAll(parentFilter);
+        SoundDefOf.Checkbox_TurnedOn.PlayOneShotOnCamera();
+      }
+      Text.Font = GameFont.Small;
+      rect.yMin = rect1.yMax;
+      TreeNode_ThingCategory node = ThingCategoryNodeDatabase.RootNode;
+      bool flag1 = true;
+      bool flag2 = true;
+      if (parentFilter != null)
+      {
+        node = parentFilter.DisplayRootCategory;
+        flag1 = parentFilter.allowedHitPointsConfigurable;
+        flag2 = parentFilter.allowedQualitiesConfigurable;
+      }
+      Rect viewRect = new Rect(0.0f, 0.0f, rect.width - 16f, ThingFilterUI.viewHeight);
+      Widgets.BeginScrollView(rect, ref scrollPosition, viewRect);
+      float y = 2f;
+      if (flag1 && !forceHideHitPointsConfig)
+        ThingFilterUI.DrawHitPointsFilterConfig(ref y, viewRect.width, filter);
+      if (flag2)
+        ThingFilterUI.DrawQualityFilterConfig(ref y, viewRect.width, filter);
+      float num2 = y;
+      Rect rect2 = new Rect(0.0f, y, viewRect.width, 9999f);
+      Listing_TreeThingFilter listingTreeThingFilter = new Listing_TreeThingFilter(filter, parentFilter, forceHiddenDefs, forceHiddenFilters, suppressSmallVolumeTags);
+      listingTreeThingFilter.Begin(rect2);
+      listingTreeThingFilter.DoCategoryChildren(node, 0, openMask, map, true);
+      listingTreeThingFilter.End();
+      if (Event.current.type == EventType.Layout)
+        ThingFilterUI.viewHeight = (float) ((double) num2 + (double) listingTreeThingFilter.CurHeight + 90.0);
+      Widgets.EndScrollView();
+    }
+
+    private static void DrawHitPointsFilterConfig(ref float y, float width, ThingFilter filter)
+    {
+      Rect rect = new Rect(20f, y, width - 20f, 28f);
+      FloatRange hitPointsPercents = filter.AllowedHitPointsPercents;
+      ref FloatRange local = ref hitPointsPercents;
+      Widgets.FloatRange(rect, 1, ref local, labelKey: "HitPoints", valueStyle: ToStringStyle.PercentZero);
+      filter.AllowedHitPointsPercents = hitPointsPercents;
+      y += 28f;
+      y += 5f;
+      Text.Font = GameFont.Small;
+    }
+
+    private static void DrawQualityFilterConfig(ref float y, float width, ThingFilter filter)
+    {
+      Rect rect = new Rect(20f, y, width - 20f, 28f);
+      QualityRange allowedQualityLevels = filter.AllowedQualityLevels;
+      ref QualityRange local = ref allowedQualityLevels;
+      Widgets.QualityRange(rect, 876813230, ref local);
+      filter.AllowedQualityLevels = allowedQualityLevels;
+      y += 28f;
+      y += 5f;
+      Text.Font = GameFont.Small;
+    }
+  }
+}
